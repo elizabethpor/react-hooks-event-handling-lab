409fa28ffe450f6b2a90450c485b29341c099ecb
Elizabeth <elizabeth.por@gmail.com>
2022-02-23 18:20:23 -0600

    assignment completed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+    
+    return (
+        <button      
+            onFocus={() => console.log('Good!')}
+            onBlur={() => console.log('Hey! Eyes on me!')}
+            // onClick={() => {
+            // focus();
+            // blur();
+        >
+        Eyes on me
+        </button>
+    )
+};
+
+export default EyesOnMe;
+
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
-// Code Keypad Component Here
+import React from "react";
+
+function Keypad() {
+    function handleChange() {
+        console.log("Entering password...");
+    }
+    return (
+        <input
+            type="password"
+            name="passwordBox"
+            onChange={handleChange}
+            placeholder="Enter the password"
+        />
+    )
+};
+
+export default Keypad;
